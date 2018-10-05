543cec27d974b447bef29ab13db4d816d88df0e7
Tobias Kremer <t.kremer@reply.de>
2018-10-05 11:14:58 +0200

    add main class
    This class is the basic entry point of the application. From here we
    will start bringing up everything we need.

---
+++ src/main/java/de/comsystoreply/spring/core/bootcamp/Application.java
@@ -0,0 +1,20 @@
+package de.comsystoreply.spring.core.bootcamp;
+
+import java.util.logging.Logger;
+
+/**
+ * Main class of the application.
+ */
+public class Application {
+
+    private static final Logger LOGGER = Logger.getLogger(Application.class.getCanonicalName());
+
+    /**
+     * Main method that acts as the entry point into the application.
+     *
+     * @param args the command line arguments the application is started with
+     */
+    public static void main(String... args) {
+        LOGGER.info("Application is running.");
+    }
+}
