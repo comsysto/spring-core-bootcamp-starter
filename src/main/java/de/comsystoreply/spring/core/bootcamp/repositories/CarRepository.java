3211a6f51a5eaba2ed338bc110ea1143c05abe86
cs-mob <cs-mob@comsysto.com>
2019-03-28 12:23:56 +0100

    talked a lot...

---
+++ src/main/java/de/comsystoreply/spring/core/bootcamp/repositories/CarRepository.java
@@ -0,0 +1,7 @@
+package de.comsystoreply.spring.core.bootcamp.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends {
+}
--- src/main/java/de/comsystoreply/spring/core/bootcamp/services/CarService.java
+++ src/main/java/de/comsystoreply/spring/core/bootcamp/services/CarService.java
@@ -1,10 +1,13 @@
 package de.comsystoreply.spring.core.bootcamp.services;
 
 import de.comsystoreply.spring.core.bootcamp.model.Car;
+import de.comsystoreply.spring.core.bootcamp.repositories.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CarService {
 
     private CarRepository repository;
