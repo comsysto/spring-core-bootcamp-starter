e8ddda8850f462d2de4e0f5de7b7c85b5a11fb31
cs-mob <cs-mob@comsysto.com>
2019-03-28 15:11:08 +0100

    added EntityManager, DataSource, JPAAdapter

--- src/main/java/de/comsystoreply/spring/core/bootcamp/repositories/RepositoryConfiguration.java
+++ src/main/java/de/comsystoreply/spring/core/bootcamp/repositories/RepositoryConfiguration.java
@@ -1,11 +1,53 @@
 package de.comsystoreply.spring.core.bootcamp.repositories;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+
 
 @Configuration
 @ComponentScan
 @EnableJpaRepositories
 public class RepositoryConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .build();
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(Environment environment) {
+        // provides a HibernatePersistenceProvider
+        HibernateJpaVendorAdapter bean = new HibernateJpaVendorAdapter();
+        bean.setGenerateDdl(environment.getProperty("spring.jpa.generate-ddl", Boolean.TYPE, false));
+        bean.setShowSql(environment.getProperty("spring.jpa.show-sql", Boolean.TYPE, false));
+
+        return bean;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            DataSource dataSource,
+            JpaVendorAdapter jpaVendorAdapter
+    ) {
+        // manages the PersistenceUnits
+        LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+        bean.setPackagesToScan("de.comsystoreply.spring.core.bootcamp.model");
+        bean.setDataSource(dataSource);
+        bean.setJpaVendorAdapter(jpaVendorAdapter);
+
+        return bean;
+    }
+
 }
