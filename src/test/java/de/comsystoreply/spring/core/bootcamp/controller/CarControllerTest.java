53a18ca6a757af75c8af42c7ee2bd79e7970fbe3
cs-mob <cs-mob@comsysto.com>
2019-03-29 10:08:05 +0100

    Add configuration for CarControllerTest

--- src/test/java/de/comsystoreply/spring/core/bootcamp/controller/CarControllerTest.java
+++ src/test/java/de/comsystoreply/spring/core/bootcamp/controller/CarControllerTest.java
@@ -1,8 +1,15 @@
 package de.comsystoreply.spring.core.bootcamp.controller;
 
+import de.comsystoreply.spring.core.bootcamp.ApplicationInitializer;
+import de.comsystoreply.spring.core.bootcamp.repositories.CarRepository;
+import de.comsystoreply.spring.core.bootcamp.repositories.RepositoryConfiguration;
+import de.comsystoreply.spring.core.bootcamp.services.ServicesConfiguration;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
@@ -10,14 +17,21 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(
         loader = AnnotationConfigContextLoader.class,
-        classes = {WebConfiguration.class}
+        classes = {WebConfiguration.class,
+                ServicesConfiguration.class}
 )
+@Configuration
 class CarControllerTest {
     @Autowired
     private CarController carController;
 
+    @Bean
+    public CarRepository carRepository(){
+        return null;
+    }
+
     @Test
     void hasAutowired() {
-        // TODO: ...
+        Assertions.assertNotNull(carController);
     }
 }
