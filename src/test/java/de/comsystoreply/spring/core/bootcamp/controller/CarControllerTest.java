7dac84f37f723b5ceea196d82c0ea69eb0c5001e
cs-mob <cs-mob@comsysto.com>
2019-03-29 10:21:27 +0100

    integrated Mockito to mock CarRepo

--- src/test/java/de/comsystoreply/spring/core/bootcamp/controller/CarControllerTest.java
+++ src/test/java/de/comsystoreply/spring/core/bootcamp/controller/CarControllerTest.java
@@ -4,9 +4,13 @@ import de.comsystoreply.spring.core.bootcamp.ApplicationInitializer;
 import de.comsystoreply.spring.core.bootcamp.repositories.CarRepository;
 import de.comsystoreply.spring.core.bootcamp.repositories.RepositoryConfiguration;
 import de.comsystoreply.spring.core.bootcamp.services.ServicesConfiguration;
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,9 +29,17 @@ class CarControllerTest {
     @Autowired
     private CarController carController;
 
+    @Mock
+    private CarRepository carRepository;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Bean
     public CarRepository carRepository(){
-        return null;
+        return carRepository;
     }
 
     @Test
