f33683e86179c5ec2cb76949ed5073289dfaa34d
cs-mob <cs-mob@comsysto.com>
2019-03-29 14:30:57 +0100

    make CarRepository transactional

--- src/main/java/de/comsystoreply/spring/core/bootcamp/repositories/CarRepository.java
+++ src/main/java/de/comsystoreply/spring/core/bootcamp/repositories/CarRepository.java
@@ -3,5 +3,8 @@ package de.comsystoreply.spring.core.bootcamp.repositories;
 import de.comsystoreply.spring.core.bootcamp.repositories.model.CarEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
+
+@Transactional
 public interface CarRepository extends JpaRepository<CarEntity, Long> {
 }
