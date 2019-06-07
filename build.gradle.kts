a7437eecbd9b91d21d0c3a97e896d856f5e8813c
Tobias Kremer <t.kremer@reply.de>
2019-06-07 13:43:13 +0200

    move to Kotlin Gradle build file
    Syntax and autocompletion in IntelliJ is much better when useing the newer Kotlin
    DSL. So it should be easier when we have to hack something on the fly in
    the training.

---
+++ build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    java
+    application
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_12
+}
+
+application {
+    mainClassName = "de.comsystoreply.spring.core.bootcamp.Application"
+}
+
+repositories {
+    jcenter()
+    mavenCentral()
+}
+
+dependencies {
+    /*
+     * Spring Framework and Testing support.
+     */
+    val springVersion = "5.1.5.RELEASE"
+    implementation("org.springframework:spring-webmvc:$springVersion")
+    testImplementation("org.springframework:spring-test:$springVersion")
+
+    /*
+     * Embedded Apache Tomcat to run the application on.
+     */
+    implementation("org.apache.tomcat.embed:tomcat-embed-jasper:9.0.16")
+
+    /*
+     * Jackson to serialize and deserialize JSON.
+     */
+    val jacksonVersion = "2.9.8"
+    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:$jacksonVersion")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:$jacksonVersion")
+
+    /*
+     * Spring Data JPA to access data using JPA
+     */
+    implementation("org.springframework.data:spring-data-jpa:2.1.5.RELEASE")
+    implementation("org.hibernate:hibernate-core:5.4.1.Final")
+    implementation("com.h2database:h2:1.4.199")
+    implementation("org.glassfish.jaxb:jaxb-runtime:2.3.2")
+
+    /*
+     * Logback behind SLF4J for logging
+     */
+    implementation("org.slf4j:slf4j-api:1.7.26")
+    runtime("ch.qos.logback:logback-classic:1.2.3")
+
+    /*
+     * JUnit test framework
+     */
+    val junitVersion = "5.4.1"
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+
+    /*
+     * Library with matchers to be used as part of test cases
+     */
+    testImplementation("org.hamcrest:hamcrest-junit:2.0.0.0")
+
+    /*
+     * JsonPath used to evaluate values in JSON responses as part of test cases
+     */
+    testImplementation("com.jayway.jsonpath:json-path-assert:2.4.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
