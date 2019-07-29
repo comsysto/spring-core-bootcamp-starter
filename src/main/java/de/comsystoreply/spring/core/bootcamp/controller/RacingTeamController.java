5737c6ce030bebaf63ee782ad19d11949b983a87
Springmob4 <springmob4@comsysto.com>
2019-07-29 14:35:35 +0200

    WIP

--- src/main/java/de/comsystoreply/spring/core/bootcamp/controller/RacingTeamController.java
+++ src/main/java/de/comsystoreply/spring/core/bootcamp/controller/RacingTeamController.java
@@ -1,27 +1,36 @@
 package de.comsystoreply.spring.core.bootcamp.controller;
 
 import de.comsystoreply.spring.core.bootcamp.racingteam.RacingTeam;
+import de.comsystoreply.spring.core.bootcamp.repo.RacingTeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 
 @RestController
 @RequestMapping(value = "/racingteam", produces = APPLICATION_JSON_UTF8_VALUE)
 public class RacingTeamController {
+    
+    @Autowired
+    private RacingTeamRepository racingTeamRepo;
 
     @GetMapping("/{id}")
     public ResponseEntity<RacingTeam> get(@PathVariable long id) {
-
-        return ResponseEntity.ok(new RacingTeam(id,"Name", List.of(), List.of()));
+        Optional<RacingTeam> racingTeam = racingTeamRepo.findById(id);
+        return ResponseEntity.ok(racingTeam.orElse(null));
     }
 
     @GetMapping
     public ResponseEntity<List< RacingTeam>> list() {
-
-        return ResponseEntity.ok(List.of());
+        Iterable<RacingTeam> all = racingTeamRepo.findAll();
+        new ArrayList(all);
+        Collections.
+        return ResponseEntity.ok(List.);
     }
 
     @PostMapping
---
+++ src/main/java/de/comsystoreply/spring/core/bootcamp/repo/RacingTeamRepository.java
@@ -0,0 +1,9 @@
+package de.comsystoreply.spring.core.bootcamp.repo;
+
+import de.comsystoreply.spring.core.bootcamp.racingteam.RacingTeam;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RacingTeamRepository extends CrudRepository<RacingTeam,Long> {
+
+
+}
