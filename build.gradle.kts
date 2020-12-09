0baf648ce215fdf11ff68fea2c14257fdc926623
Daniel Bartl <d.bartl@comsysto.com>
2020-12-09 19:37:52 +0100

    upgraded the rest of the dependencies to their latest versions like junit, mockito, tomcat etc.

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,13 +27,13 @@ dependencies {
     /*
      * Embedded Apache Tomcat to run the application on.
      */
-    implementation("org.apache.tomcat.embed:tomcat-embed-jasper:9.0.21")
+    implementation("org.apache.tomcat.embed:tomcat-embed-jasper:9.0.40")
     implementation("com.sun.activation:javax.activation:1.2.0")
 
     /*
      * Jackson to serialize and deserialize JSON.
      */
-    val jacksonVersion = "2.9.9"
+    val jacksonVersion = "2.12.0"
     implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:$jacksonVersion")
@@ -42,25 +42,25 @@ dependencies {
     /*
      * Logback behind SLF4J for logging
      */
-    implementation("org.slf4j:slf4j-api:1.7.26")
+    implementation("org.slf4j:slf4j-api:1.7.30")
     runtimeOnly("ch.qos.logback:logback-classic:1.2.3")
 
     /*
      * JUnit test framework
      */
-    val junitVersion = "5.4.2"
+    val junitVersion = "5.7.0"
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 
     /*
      * Library with matchers to be used as part of test cases
      */
-    testImplementation("org.hamcrest:hamcrest-junit:2.0.0.0")
+    testImplementation("org.hamcrest:hamcrest:2.2")
 
     /*
      * Add Mockito mocking framework.
      */
-    testImplementation("org.mockito:mockito-core:2.28.2")
+    testImplementation("org.mockito:mockito-core:3.6.28")
 
     /*
      * JsonPath used to evaluate values in JSON responses as part of test cases
