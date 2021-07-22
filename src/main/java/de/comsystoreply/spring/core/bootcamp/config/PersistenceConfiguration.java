14d5c32d4f0c458d62637792ed3705fbf467d1b3
sfleischer <s.fleischer@reply.de>
2021-07-22 14:49:49 +0200

    PIW

--- src/main/java/de/comsystoreply/spring/core/bootcamp/config/PersistenceConfiguration.java
+++ src/main/java/de/comsystoreply/spring/core/bootcamp/config/PersistenceConfiguration.java
@@ -2,4 +2,26 @@ package de.comsystoreply.spring.core.bootcamp.config;
 
 public class PersistenceConfiguration {
 
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(Environment environment) {
+        // provides a HibernatePersistenceProvider
+        HibernateJpaVendorAdapter bean = new HibernateJpaVendorAdapter();
+        bean.setGenerateDdl(environment.getProperty("spring.jpa.generate-ddl", Boolean.TYPE, false));
+        bean.setShowSql(environment.getProperty("spring.jpa.show-sql", Boolean.TYPE, false));
+
+        return bean;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            DataSource dataSource,
+            JpaVendorAdapter jpaVendorAdapter
+    ) {
+        // manages the PersistenceUnits
+        LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+        bean.setDataSource(dataSource);
+        bean.setJpaVendorAdapter(jpaVendorAdapter);
+
+        return bean;
+    }
 }
