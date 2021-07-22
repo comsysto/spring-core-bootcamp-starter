e6f5f3ad3b61a02e98aa2f6e6450131cc8664abe
sfleischer <s.fleischer@reply.de>
2021-07-22 16:00:23 +0200

    Changes by tobiasglas

--- src/main/java/de/comsystoreply/spring/core/bootcamp/rest/RacingTeamController.java
+++ src/main/java/de/comsystoreply/spring/core/bootcamp/rest/RacingTeamController.java
@@ -2,6 +2,7 @@ package de.comsystoreply.spring.core.bootcamp.rest;
 
 import de.comsystoreply.spring.core.bootcamp.model.RacingTeam;
 import de.comsystoreply.spring.core.bootcamp.persistence.RacingTeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
