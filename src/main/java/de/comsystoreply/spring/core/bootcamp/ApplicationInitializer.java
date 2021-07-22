9736e99ff68bb8c82d026603ee9024ad7537bd6e
sfleischer <s.fleischer@reply.de>
2021-07-22 16:00:05 +0200

    Piw

--- src/main/java/de/comsystoreply/spring/core/bootcamp/ApplicationInitializer.java
+++ src/main/java/de/comsystoreply/spring/core/bootcamp/ApplicationInitializer.java
@@ -6,6 +6,7 @@ import javax.servlet.ServletException;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
