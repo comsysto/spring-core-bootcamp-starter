0879d0ad80c6e54aad13a6b11ca8dc0ca83e12bf
sfleischer <s.fleischer@reply.de>
2021-07-22 15:00:03 +0200

    Wip

--- src/main/java/de/comsystoreply/spring/core/bootcamp/config/DataSourceConfiguration.java
+++ src/main/java/de/comsystoreply/spring/core/bootcamp/config/DataSourceConfiguration.java
@@ -17,4 +17,27 @@ public class DataSourceConfiguration {
     dataSource.setPassword("inTheSh4d0ws");
     return dataSource;
   }
+
+  @Bean
+  public JpaVendorAdapter jpaVendorAdapter(Environment environment) {
+    // provides a HibernatePersistenceProvider
+    HibernateJpaVendorAdapter bean = new HibernateJpaVendorAdapter();
+    // bean.setGenerateDdl(environment.getProperty("spring.jpa.generate-ddl", Boolean.TYPE, false));
+    // bean.setShowSql(environment.getProperty("spring.jpa.show-sql", Boolean.TYPE, false));
+
+    return bean;
+  }
+
+  @Bean
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+          DataSource dataSource,
+          JpaVendorAdapter jpaVendorAdapter
+  ) {
+    // manages the PersistenceUnits
+    LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+    bean.setDataSource(dataSource);
+    bean.setJpaVendorAdapter(jpaVendorAdapter);
+
+    return bean;
+  }
 }
--- src/main/java/de/comsystoreply/spring/core/bootcamp/rest/RacingTeamController.java
+++ src/main/java/de/comsystoreply/spring/core/bootcamp/rest/RacingTeamController.java
@@ -3,8 +3,17 @@ package de.comsystoreply.spring.core.bootcamp.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+
 @RestController
+@PersistenceContext(PersistenceContextType.EXTENDED)
 public class RacingTeamController {
+
+  @PersistenceContext(PersistenceContextType.EXTENDED)
+  private EntityManager entityManager;
+
 
   @GetMapping("/teams")
   public String getTeams() {
