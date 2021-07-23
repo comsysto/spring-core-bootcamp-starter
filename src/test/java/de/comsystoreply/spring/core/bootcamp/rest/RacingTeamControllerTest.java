6d7af62af6a2118f2cb60a1e663255bf11f78cf9
sfleischer <s.fleischer@reply.de>
2021-07-23 10:31:01 +0200

    Wip

--- src/test/java/de/comsystoreply/spring/core/bootcamp/rest/RacingTeamControllerTest.java
+++ src/test/java/de/comsystoreply/spring/core/bootcamp/rest/RacingTeamControllerTest.java
@@ -10,8 +10,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalDate;
 import java.util.List;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
@@ -23,9 +26,14 @@ class RacingTeamControllerTest {
 
     @Test
     void getTeams() throws Exception {
-        when(repository.findAll()).thenReturn(List.of(new RacingTeam(5, "racing-team", List.of(new Driver(1, "best", "racerX", LocalDate.now(), ....)))));
+        RacingTeam racingTeam = new RacingTeam(6L, "racing-team2", List.of());
+        Driver driver = new Driver(1L, "best", "racerX", LocalDate.now(), racingTeam);
+        racingTeam.setDrivers(List.of(driver));
+        when(repository.findAll()).thenReturn(List.of(racingTeam));
 
         mock.perform(get("/teams").accept(APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].id"), equalTo(racingTeam.getId()));
     }
 }
