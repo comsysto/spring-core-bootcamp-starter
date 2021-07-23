8ce75aff13a84d4133ea7329247b9c0f68d86878
sfleischer <s.fleischer@reply.de>
2021-07-23 10:18:56 +0200

    Wip

--- src/test/java/de/comsystoreply/spring/core/bootcamp/rest/RacingTeamControllerTest.java
+++ src/test/java/de/comsystoreply/spring/core/bootcamp/rest/RacingTeamControllerTest.java
@@ -1,10 +1,14 @@
 package de.comsystoreply.spring.core.bootcamp.rest;
 
+import de.comsystoreply.spring.core.bootcamp.model.RacingTeam;
 import de.comsystoreply.spring.core.bootcamp.persistence.RacingTeamRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -15,10 +19,11 @@ class RacingTeamControllerTest {
 
     private RacingTeamRepository repository = Mockito.mock(RacingTeamRepository.class);
     private RacingTeamController rtc = new RacingTeamController(repository);
-    private MockMvc mock = standaloneSetup(RacingTeamController.class).build();
+    private MockMvc mock = standaloneSetup(rtc).build();
 
     @Test
     void getTeams() throws Exception {
+        when(repository.findAll()).thenReturn(List.of(new RacingTeam(5, "racing-team", List.of(new Driver(1, "best", "racerX", LocalDate.now(), ....)))));
 
         mock.perform(get("/teams").accept(APPLICATION_JSON))
                 .andExpect(status().isOk());
