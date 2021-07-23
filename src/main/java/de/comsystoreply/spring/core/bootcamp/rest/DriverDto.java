a85217e45ad5400038ce2e09e2dc3fbfa5f2e07f
sfleischer <s.fleischer@reply.de>
2021-07-23 10:54:33 +0200

    Wip

---
+++ src/main/java/de/comsystoreply/spring/core/bootcamp/rest/DriverDto.java
@@ -0,0 +1,70 @@
+package de.comsystoreply.spring.core.bootcamp.rest;
+
+import de.comsystoreply.spring.core.bootcamp.model.Driver;
+
+import java.time.LocalDate;
+
+public class DriverDto {
+    Long id;
+    String firstName;
+    String lastName;
+    LocalDate dateOfBirth;
+
+    public DriverDto(Long id, String firstName, String lastName, LocalDate dateOfBirth) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public DriverDto toDto(Driver driver) {
+        return new DriverDto(
+                driver.getId(),
+                driver.getFirstName(),
+                driver.getLastName(),
+                driver.getDateOfBirth()
+        )
+    }
+
+    public  Driver fromDto(DriverDto dto) {
+        return new Driver(
+                dto.getId(),
+                dto.getFirstName(),
+                dto.getLastName(),
+                dto.getDateOfBirth(),
+                null // TODO: getTeam
+        )
+    }
+}
---
+++ src/main/java/de/comsystoreply/spring/core/bootcamp/rest/RacingTeamDto.java
@@ -0,0 +1,8 @@
+package de.comsystoreply.spring.core.bootcamp.rest;
+
+public class RacingTeamDto {
+
+    Long id;
+    String name;
+    List<DriverDto> drivers;
+}
