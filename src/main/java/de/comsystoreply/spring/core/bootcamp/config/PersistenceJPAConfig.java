56790c661077a6eb28fd83a66275e20d237e33fc
fabifuchs <f.fuchs@reply.de>
2021-11-29 14:56:01 +0100

    add jpa config

---
+++ src/main/java/de/comsystoreply/spring/core/bootcamp/config/PersistenceJPAConfig.java
@@ -0,0 +1,43 @@
+package de.comsystoreply.spring.core.bootcamp.config;
+
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class PersistenceJPAConfig{
+
+   @Bean
+   public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+      LocalContainerEntityManagerFactoryBean em 
+        = new LocalContainerEntityManagerFactoryBean();
+      em.setDataSource(dataSource());
+      em.setPackagesToScan(new String[] { "com.baeldung.persistence.model" });
+
+      JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+      em.setJpaVendorAdapter(vendorAdapter);
+      em.setJpaProperties(additionalProperties());
+
+      return em;
+   }
+
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/spring_jpa");
+        dataSource.setUsername( "tutorialuser" );
+        dataSource.setPassword( "tutorialmy5ql" );
+        return dataSource;
+    }
+   
+}
