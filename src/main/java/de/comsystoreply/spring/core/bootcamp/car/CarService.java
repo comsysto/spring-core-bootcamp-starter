f5b9d60a457394c9f12cabcdf15ee64def43acd3
Markus Steger <m.steger@reply.de>
2021-11-29 11:43:03 +0100

    markus changes

--- src/main/java/de/comsystoreply/spring/core/bootcamp/car/CarService.java
+++ src/main/java/de/comsystoreply/spring/core/bootcamp/car/CarService.java
@@ -4,11 +4,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+
+import de.comsystoreply.spring.core.bootcamp.Application;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CarService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CarService.class);
 
     private List<Car> cars = new ArrayList<>();
 
@@ -17,7 +22,10 @@ public class CarService {
     }
 
     public void createCar(Car car) {
+
         cars.add(car);
+        LOGGER.debug("{}",cars.size());
+
     }
 
     public void createCars(List<Car> cars) {
