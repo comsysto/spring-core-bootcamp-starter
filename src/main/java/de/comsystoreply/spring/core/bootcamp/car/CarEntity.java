9d3df12d5e6631c4640b3944034a215201a8aa15
katieclark <k.clark@reply.de>
2021-11-29 14:19:45 +0100

    adding attributes to CarEntity

--- src/main/java/de/comsystoreply/spring/core/bootcamp/car/CarEntity.java
+++ src/main/java/de/comsystoreply/spring/core/bootcamp/car/CarEntity.java
@@ -1,8 +1,25 @@
 package de.comsystoreply.spring.core.bootcamp.car;
 
-import javax.persistence.Entity;
+
+import javax.persistence.*;
 
 @Entity
+@Table(name="CAR")
 public class CarEntity {
 
+    @Id
+    @SequenceGenerator(name = "car_generator",
+            sequenceName = "CAR_SEQUENCE",
+            allocationSize = 1)
+    @GeneratedValue(generator = "car_generator")
+    private Long id;
+
+    @Column(name="TITLE")
+    private String title;
+
+    @Column(name = "HORSE_POWER")
+    private int horsePower;
+
+    @Column(name = "WEIGHT_IN_KILO")
+    private float weightInKilo;
 }
