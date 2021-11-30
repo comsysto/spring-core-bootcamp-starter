da15c5e4fb0cc9f029da4b748ae048321cc06d66
Mido <mido@MacBook-Pro-von-Mido.local>
2021-11-30 15:05:43 +0100

    add profile

--- src/main/java/de/comsystoreply/spring/core/bootcamp/config/PersistenceJPAConfig.java
+++ src/main/java/de/comsystoreply/spring/core/bootcamp/config/PersistenceJPAConfig.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
@@ -45,6 +46,7 @@ public class PersistenceJPAConfig{
     }
 
     @Bean
+    @Profile("h2")
     public DataSource dataSource() {
         return new EmbeddedDatabaseBuilder()
                 .setType(EmbeddedDatabaseType.H2)
@@ -54,7 +56,7 @@ public class PersistenceJPAConfig{
     Properties additionalProperties() {
         Properties properties = new Properties();
         properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        //properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
 
         return properties;
     }
