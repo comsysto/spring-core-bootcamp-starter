580e71d8a79b390e5c920bd4daf44d1d5a3ed78a
katieclark <k.clark@reply.de>
2021-11-30 15:23:13 +0100

    postgres

--- src/main/java/de/comsystoreply/spring/core/bootcamp/config/PersistenceJPAConfig.java
+++ src/main/java/de/comsystoreply/spring/core/bootcamp/config/PersistenceJPAConfig.java
@@ -1,14 +1,18 @@
 package de.comsystoreply.spring.core.bootcamp.config;
 
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import javax.swing.text.html.parser.Entity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.EntityManagerFactoryAccessor;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -24,10 +28,10 @@ import java.util.Properties;
 public class PersistenceJPAConfig{
 
    @Bean
-   public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+   public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
       LocalContainerEntityManagerFactoryBean em 
         = new LocalContainerEntityManagerFactoryBean();
-      em.setDataSource(dataSource());
+      em.setDataSource(dataSource);
       em.setPackagesToScan(new String[] { "de.comsystoreply.spring.core.bootcamp" });
 
       JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
@@ -38,20 +42,32 @@ public class PersistenceJPAConfig{
    }
 
     @Bean
-    public PlatformTransactionManager transactionManager() {
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
 
         return transactionManager;
     }
 
     @Bean
     @Profile("h2")
-    public DataSource dataSource() {
+    public DataSource h2DataSource() {
         return new EmbeddedDatabaseBuilder()
                 .setType(EmbeddedDatabaseType.H2)
                 .build();
     }
+
+    @Bean
+    @Profile("postgres")
+    public DataSource dataSource() {
+        DriverManagerDataSource driver = new DriverManagerDataSource();
+        driver.setDriverClassName("org.postgresql.Driver");
+        driver.setUrl("jdbc:postgresql://localhost:15432/");
+        driver.setUsername("ninja-guy");
+        driver.setPassword("inTheSh4d0ws");
+        return driver;
+    }
+
 
     Properties additionalProperties() {
         Properties properties = new Properties();
