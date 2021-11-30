a4eedb178a3c903d0d54cb5908910a896ee55453
katieclark <k.clark@reply.de>
2021-11-30 11:53:58 +0100

    finishing Configuration for Car Controller Test

--- src/test/java/de/comsystoreply/spring/core/bootcamp/car/CarControllerTest.java
+++ src/test/java/de/comsystoreply/spring/core/bootcamp/car/CarControllerTest.java
@@ -1,38 +1,60 @@
 package de.comsystoreply.spring.core.bootcamp.car;
 
 import de.comsystoreply.spring.core.bootcamp.controller.CarController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-
+@WebAppConfiguration
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(
         loader = AnnotationConfigWebContextLoader.class,
         classes = {CarControllerTest.Configuration.class})
 public class CarControllerTest {
+
     @Autowired
     private WebApplicationContext applicationContext;
 
+    //note: Spring Boot offers "MockBean"
+    @Autowired
+    private CarService carService;
+
     private MockMvc mvc;
+
     @BeforeEach
     void setup() {
         this.mvc = MockMvcBuilders.webAppContextSetup(applicationContext).build();
     }
 
+    @Test
+    void doNothing(){
+        Assertions.fail();
+    }
+
     @org.springframework.context.annotation.Configuration
     static class Configuration{
         @Bean
-        public CarController carController(){
-            return null
+        public CarController carController(CarService carService) {
+            return new CarController(carService);
+        }
+
+        @Bean
+        public CarService carService() {
+            return Mockito.mock(CarService.class);
         }
     }
 
