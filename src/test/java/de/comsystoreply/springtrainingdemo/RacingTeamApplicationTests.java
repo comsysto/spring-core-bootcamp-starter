7f3d00ed8125c24c96a0b640e53924e3219f16bc
maju-csr <m.jung@reply.de>
2022-02-21 14:58:50 +0100

    create basic drivers endpoint test

--- src/test/java/de/comsystoreply/springtrainingdemo/RacingTeamApplicationTests.java
+++ src/test/java/de/comsystoreply/springtrainingdemo/RacingTeamApplicationTests.java
@@ -6,14 +6,28 @@ import de.comsystoreply.springtrainingdemo.service.DriverService;
 import de.comsystoreply.springtrainingdemo.service.RacingTeamService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.Assert;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class RacingTeamApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
     @Autowired
     private DriverRepository driverRepository;
     @Autowired
@@ -34,7 +48,16 @@ class RacingTeamApplicationTests {
     }
 
     @Test
-    void allDrivers(){
+    void allDrivers() throws Exception {
+        List<Driver> drivers = new ArrayList<Driver>();
+        drivers.addAll( List.of(new Driver("max"), new Driver("moritz")));
+
+        driverRepository.saveAll(drivers);
+
+        this.mockMvc.perform(get("/drivers")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("max")));
+
+
 
     }
 }
