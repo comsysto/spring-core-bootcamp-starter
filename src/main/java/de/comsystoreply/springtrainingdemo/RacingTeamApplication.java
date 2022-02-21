e750dc558121593f133dd1ed1f9f6a5a695d5a24
Korbinian Schleifer <korbinian.schleifer@gmail.com>
2022-02-21 10:23:44 +0100

    starter project

---
+++ src/main/java/de/comsystoreply/springtrainingdemo/RacingTeamApplication.java
@@ -0,0 +1,13 @@
+package de.comsystoreply.springtrainingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacingTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacingTeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/comsystoreply/springtrainingdemo/RacingTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package de.comsystoreply.springtrainingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacingTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
