c654061284a7ce405860ad89499d60e4551ebd6a
tien.huynhminh <tien.huynhminh@comsysto.com>
2022-02-21 11:00:48 +0100

    add first test

--- src/test/java/de/comsystoreply/springtrainingdemo/RacingTeamApplicationTests.java
+++ src/test/java/de/comsystoreply/springtrainingdemo/RacingTeamApplicationTests.java
@@ -2,12 +2,17 @@ package de.comsystoreply.springtrainingdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class RacingTeamApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void createTeam() {
+		RacingTeamService racingTeamService = new RacingTeamService();
+		racingTeamService.createTeam('Test-Team');
+		Assert.notNull(racingTeamService.createTeam('Test-Team'), null);
 	}
+
 
 }
