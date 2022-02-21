3a734b46f981c0014ba1be8beb5fad1e206f7ec3
DizzleReply <D.grimm@reply.de>
2022-02-21 12:49:38 +0100

    add save Driver tests

--- src/main/java/de/comsystoreply/springtrainingdemo/service/DriverService.java
+++ src/main/java/de/comsystoreply/springtrainingdemo/service/DriverService.java
@@ -1,15 +1,23 @@
 package de.comsystoreply.springtrainingdemo.service;
 
 import de.comsystoreply.springtrainingdemo.model.Driver;
+import de.comsystoreply.springtrainingdemo.repos.DriverRepository;
 
 public class DriverService {
+
+    private final DriverRepository driverRepository;
+
+    public DriverService(DriverRepository driverRepository) {
+        this.driverRepository = driverRepository;
+    }
 
     public Driver createDriver(String firstName) {
         return new Driver(firstName);
     }
 
     public void saveDriver(Driver driver) {
-        
+        this.driverRepository.save(driver);
     }
+
 
 }
--- src/test/java/de/comsystoreply/springtrainingdemo/RacingTeamApplicationTests.java
+++ src/test/java/de/comsystoreply/springtrainingdemo/RacingTeamApplicationTests.java
@@ -1,6 +1,10 @@
 package de.comsystoreply.springtrainingdemo;
 
+import de.comsystoreply.springtrainingdemo.model.Driver;
+import de.comsystoreply.springtrainingdemo.repos.DriverRepository;
+import de.comsystoreply.springtrainingdemo.service.DriverService;
 import de.comsystoreply.springtrainingdemo.service.RacingTeamService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
@@ -8,12 +12,29 @@ import org.springframework.util.Assert;
 @SpringBootTest
 class RacingTeamApplicationTests {
 
-	@Test
-	void createTeam() {
-		RacingTeamService racingTeamService = new RacingTeamService();
-		Assert.notNull(racingTeamService.createRacingTeam("someRacingTeam"));
+    private DriverRepository driverRepository;
 
-	}
+    @BeforeAll
+    void setup(DriverRepository driverRepository) {
+        this.driverRepository = driverRepository;
+    }
+
+    @Test
+    void createTeam() {
+        RacingTeamService racingTeamService = new RacingTeamService();
+        Assert.notNull(racingTeamService.createRacingTeam("someRacingTeam"));
+
+    }
+
+    @Test
+    void saveDriver() {
+        DriverService driverService = new DriverService(driverRepository);
+        Driver driver = new Driver("Max");
+
+        driverService.saveDriver(driver);
+        Assert.isTrue(driverRepository.findById(driver.getId()).isPresent());
+
+    }
 
 
 }
