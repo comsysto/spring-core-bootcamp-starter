c2124d9368f2cb6ce8f9144af33a9e9b8311e048
Benjamin Hauck <benjamin.hauck@posteo.net>
2022-02-22 10:03:24 +0100

    benj

---
+++ src/main/java/de/comsystoreply/springtrainingdemo/config/DriverConfig.java
@@ -0,0 +1,9 @@
+package de.comsystoreply.springtrainingdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DriverConfig {
+    @Bean public DriverConfig(){};
+}
