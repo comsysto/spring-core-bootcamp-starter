1b99cd3be88a8da021a810b3aa436d60351566d8
Benjamin Hauck <benjamin.hauck@posteo.net>
2022-02-22 14:26:44 +0100

    mob

--- src/main/java/de/comsystoreply/springtrainingdemo/RacingTeamApplication.java
+++ src/main/java/de/comsystoreply/springtrainingdemo/RacingTeamApplication.java
@@ -2,8 +2,10 @@ package de.comsystoreply.springtrainingdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@Profile("prod")
 public class RacingTeamApplication {
 
 	public static void main(String[] args) {
