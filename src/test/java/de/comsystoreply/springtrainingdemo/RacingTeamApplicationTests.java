6e2528055d491be8aee133d6a34874c0ffe81391
rib-reply <i.bartsch@reply.de>
2022-02-22 14:08:12 +0100

    add profile

--- src/test/java/de/comsystoreply/springtrainingdemo/RacingTeamApplicationTests.java
+++ src/test/java/de/comsystoreply/springtrainingdemo/RacingTeamApplicationTests.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
@@ -30,6 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("dev")
 class RacingTeamApplicationTests {
 
     @Autowired
