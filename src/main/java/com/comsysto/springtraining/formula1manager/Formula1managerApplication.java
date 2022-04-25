cc469d56a79525fa598e90b2b8b5e975e17794f4
Daniel Schneider <d.schneider@reply.de>
2022-04-25 10:13:36 +0200

    added Spring initializer

---
+++ src/main/java/com/comsysto/springtraining/formula1manager/Formula1managerApplication.java
@@ -0,0 +1,13 @@
+package com.comsysto.springtraining.formula1manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Formula1managerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Formula1managerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comsysto/springtraining/formula1manager/Formula1managerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comsysto.springtraining.formula1manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Formula1managerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
