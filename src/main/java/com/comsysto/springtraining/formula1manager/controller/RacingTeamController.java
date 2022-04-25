6cdba4ed4792b910755d38ab0c73841df86c6e33
skennic <skennic@gmail.com>
2022-04-25 10:50:36 +0200

    WIP

--- src/main/java/com/comsysto/springtraining/formula1manager/controller/RacingTeamController.java
+++ src/main/java/com/comsysto/springtraining/formula1manager/controller/RacingTeamController.java
@@ -1,18 +1,25 @@
 package com.comsysto.springtraining.formula1manager.controller;
 
 import com.comsysto.springtraining.formula1manager.model.RacingTeam;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/racingteams")
 public class RacingTeamController {
-    @GetMapping("/")
+    @GetMapping
     public List<RacingTeam> getAllRacingTeams () {
         List<RacingTeam> teams = new ArrayList<>();
         teams.add(new RacingTeam(1L, "test"));
         return teams;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public RacingTeam createRacingTeam(@RequestBody RacingTeam team) {
+        return team;
     }
 }
