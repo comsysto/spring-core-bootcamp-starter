1f039a5eeee0dc0b10718607fd249cbd545aaf52
m.dunker <m.dunker@reply.de>
2022-04-25 10:36:29 +0200

    finish controller

--- src/main/java/com/comsysto/springtraining/formula1manager/controller/RacingTeamController.java
+++ src/main/java/com/comsysto/springtraining/formula1manager/controller/RacingTeamController.java
@@ -4,12 +4,15 @@ import com.comsysto.springtraining.formula1manager.model.RacingTeam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class RacingTeamController {
     @GetMapping("/")
     public List<RacingTeam> getAllRacingTeams () {
-        return
+        List<RacingTeam> teams = new ArrayList<>();
+        teams.add(new RacingTeam(1L, "test"));
+        return teams;
     }
 }
--- src/main/java/com/comsysto/springtraining/formula1manager/model/RacingTeam.java
+++ src/main/java/com/comsysto/springtraining/formula1manager/model/RacingTeam.java
@@ -1,5 +1,10 @@
 package com.comsysto.springtraining.formula1manager.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class RacingTeam {
     private Long id;
     private String name;
