b71e2a84f964c108839a28c56ab08a9ee6bc67ad
Daniel Schneider <d.schneider@reply.de>
2022-04-26 12:00:22 +0200

    next

--- src/main/java/com/comsysto/springtraining/formula1manager/Formula1managerApplication.java
+++ src/main/java/com/comsysto/springtraining/formula1manager/Formula1managerApplication.java
@@ -11,14 +11,13 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-
-import java.time.Instant;
 import java.util.UUID;
 
 @SpringBootApplication
@@ -28,24 +27,24 @@ public class Formula1managerApplication {
         SpringApplication.run(Formula1managerApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner setStartupData(RacingTeamRepository racingTeamRepository, DriverRepository driverRepository) {
-        return args -> {
-            RacingTeam team1 = new RacingTeam(UUID.fromString("2b674d9d-6d41-418f-ac49-560de47bd81d"), "Team 1");
-            var resultingTeam = racingTeamRepository.save(team1);
-
-            Driver driver1 = new Driver(UUID.fromString("2b674d9d-6d41-418f-ac49-560de47bd81d"),
-                    "first",
-                    "last",
-                    5,
-                ZonedDateTime.of(1990, 2, 1, 0, 0, 0, 0, ZoneId.of("America/Los_Angeles")), resultingTeam);
-            driverRepository.save(driver1);
-        };
-    }
+//    @Bean
+//    public CommandLineRunner setStartupData(RacingTeamRepository racingTeamRepository, DriverRepository driverRepository) {
+//        return args -> {
+//            RacingTeam team1 = new RacingTeam(UUID.fromString("2b674d9d-6d41-418f-ac49-560de47bd81d"), "Team 1");
+//            var resultingTeam = racingTeamRepository.save(team1);
+//
+//            Driver driver1 = new Driver(UUID.fromString("2b674d9d-6d41-418f-ac49-560de47bd81d"),
+//                    "first",
+//                    "last",
+//                    5,
+//                ZonedDateTime.of(1990, 2, 1, 0, 0, 0, 0, ZoneId.of("America/Los_Angeles")), resultingTeam);
+//            driverRepository.save(driver1);
+//        };
+//    }
 
     @Bean
     @Profile("America")
-    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
-        return builder.createXmlMapper(false).timeZone("America/Los_Angeles").build();
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder, @Value("$app.timezone") String timezone) {
+        return builder.createXmlMapper(false).timeZone(timezone).build();
     }
 }
