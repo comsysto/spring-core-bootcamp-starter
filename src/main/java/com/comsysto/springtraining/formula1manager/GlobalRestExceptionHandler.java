699a934ac0678844d97ca5a56e0665e36fcd9252
Nadia Ilyina <n.ilyina@reply.de>
2022-04-26 14:47:54 +0200

    done

--- src/main/java/com/comsysto/springtraining/formula1manager/GlobalRestExceptionHandler.java
+++ src/main/java/com/comsysto/springtraining/formula1manager/GlobalRestExceptionHandler.java
@@ -1,17 +1,19 @@
 package com.comsysto.springtraining.formula1manager;
 
+import java.util.Set;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 class GlobalRestExceptionHandler {
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler
-    public BindingResult handleException(MethodArgumentNotValidException exception) {
-        return exception.getBindingResult();
+    @ResponseBody
+    public Object handleException(MethodArgumentNotValidException exception) {
+        return Set.of(exception.getBindingResult().getAllErrors());
     }
 }
