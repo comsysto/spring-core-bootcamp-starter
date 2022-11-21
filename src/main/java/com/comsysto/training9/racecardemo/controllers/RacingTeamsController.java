18880833d51d3c08366d9103bed94f8cea8bda0c
tinrupcic <t.rupcic@reply.de>
2022-11-21 14:27:10 +0100

    creating RacingTeamRepository

--- src/main/java/com/comsysto/training9/racecardemo/controllers/RacingTeamsController.java
+++ src/main/java/com/comsysto/training9/racecardemo/controllers/RacingTeamsController.java
@@ -1,24 +1,28 @@
 package com.comsysto.training9.racecardemo.controllers;
 
+import com.comsysto.training9.racecardemo.dbinterface.RacingTeamRepository;
 import com.comsysto.training9.racecardemo.model.RacingTeamModel;
+import java.util.List;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping()
 public class RacingTeamsController {
 
+  RacingTeamRepository racingTeamRepository;
+
   @GetMapping("/racingteam/{id}")
   public RacingTeamModel getRacingTeam(@PathVariable long id){
+
+    racingTeamRepository.findAll();
 
     return new RacingTeamModel(id, "TeamSuperb");
   }
 
   @PutMapping("/racingteam")
   public RacingTeamModel putRacingTeam(@RequestBody RacingTeamModel racingTeamModel){
-    //TODO save it somewhere
 
-
-    return racingTeamModel;
+    return  racingTeamRepository.save(racingTeamModel);
   }
 
 
---
+++ src/main/java/com/comsysto/training9/racecardemo/dbinterface/RacingTeamRepository.java
@@ -0,0 +1,31 @@
+package com.comsysto.training9.racecardemo.dbinterface;
+
+import com.comsysto.training9.racecardemo.model.RacingTeamModel;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.context.annotation.ApplicationScope;
+
+@Repository
+public class RacingTeamRepository {
+    private List<RacingTeamModel> racingTeamModelList = new ArrayList<>();
+
+    public RacingTeamModel save(RacingTeamModel racingTeamModel){
+        racingTeamModelList.add(racingTeamModel);
+        return racingTeamModel;
+    }
+
+    public List<RacingTeamModel> findAll() {
+
+        return racingTeamModelList;
+    }
+
+    public RacingTeamModel findById(long id) {
+        for(RacingTeamModel racingTeamRepository: racingTeamModelList){
+            if(racingTeamRepository.getId()==id){
+                return racingTeamRepository;
+            }
+        }
+        return null;
+    }
+}
