a68c341c57ce47fef2b8e1a824cb17421149cb6a
Vigerl, Michael (StMELF-Extern Fa. ComsystoReply) <Michael.Vigerl@stmelf.bayern.de>
2022-11-21 13:55:18 +0100

    get all

--- src/main/java/com/comsysto/training9/racecardemo/controllers/RacingTeamsController.java
+++ src/main/java/com/comsysto/training9/racecardemo/controllers/RacingTeamsController.java
@@ -2,28 +2,32 @@ package com.comsysto.training9.racecardemo.controllers;
 
 import com.comsysto.training9.racecardemo.dbinterface.RacingTeamRepository;
 import com.comsysto.training9.racecardemo.model.RacingTeamModel;
+
 import java.util.List;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping()
+@RequestMapping("/racingteam")
 public class RacingTeamsController {
-  RacingTeamRepository racingTeamRepository;
+    RacingTeamRepository racingTeamRepository;
 
-  public RacingTeamsController(RacingTeamRepository racingTeamRepository) {
-    this.racingTeamRepository = racingTeamRepository;
-  }
+    public RacingTeamsController(RacingTeamRepository racingTeamRepository) {
+        this.racingTeamRepository = racingTeamRepository;
+    }
 
-  @GetMapping("/racingteam/{id}")
-  public RacingTeamModel getRacingTeam(@PathVariable long id){
-    return racingTeamRepository.findById(id);
-  }
+    @GetMapping("{id}")
+    public RacingTeamModel getRacingTeam(@PathVariable long id) {
+        return racingTeamRepository.findById(id);
+    }
 
-  @PutMapping("/racingteam")
-  public RacingTeamModel putRacingTeam(@RequestBody RacingTeamModel racingTeamModel){
+    @GetMapping()
+    public List<RacingTeamModel> getRacingTeams() {
+        return racingTeamRepository.findAll();
+    }
 
-    return  racingTeamRepository.save(racingTeamModel);
-  }
-
-
+    @PutMapping()
+    public RacingTeamModel putRacingTeam(@RequestBody RacingTeamModel racingTeamModel) {
+        return racingTeamRepository.save(racingTeamModel);
+    }
 }
