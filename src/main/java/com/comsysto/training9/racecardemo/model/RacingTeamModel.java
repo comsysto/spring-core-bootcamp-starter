df0601b147ac131c1520f76699887f7e5459136e
tinrupcic <t.rupcic@reply.de>
2022-11-21 15:59:36 +0100

    creating Entity

--- src/main/java/com/comsysto/training9/racecardemo/model/RacingTeamModel.java
+++ src/main/java/com/comsysto/training9/racecardemo/model/RacingTeamModel.java
@@ -1,9 +1,12 @@
 package com.comsysto.training9.racecardemo.model;
 
+import javax.persistence.Entity;
 import lombok.Value;
 
 @Value
+@Entity
 public class RacingTeamModel {
+
     Long id;
     String name;
 }
