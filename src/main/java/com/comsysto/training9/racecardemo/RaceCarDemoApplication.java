7fa92d74c8a2721c814456b6b2d3bc2d247fb3be
Vigerl, Michael (StMELF-Extern Fa. ComsystoReply) <Michael.Vigerl@stmelf.bayern.de>
2022-11-21 09:48:56 +0100

    sprint init

---
+++ src/main/java/com/comsysto/training9/racecardemo/RaceCarDemoApplication.java
@@ -0,0 +1,13 @@
+package com.comsysto.training9.racecardemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceCarDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaceCarDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comsysto/training9/racecardemo/RaceCarDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comsysto.training9.racecardemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceCarDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
