63bf8ac679ce18368a0d9de37001410acd72cf9a
Tobias Irl <t.irl@reply.de>
2022-11-22 11:32:14 +0100

    Interface

---
+++ src/main/java/com/comsysto/training9/racecardemo/services/RacingTeamDataService.java
@@ -0,0 +1,13 @@
+package com.comsysto.training9.racecardemo.services;
+
+import com.comsysto.training9.racecardemo.controllers.model.RacingTeamModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface RacingTeamDataService {
+
+    public RacingTeamModel save(RacingTeamModel racingTeamModel);
+    public List<RacingTeamModel> findAll();
+    public RacingTeamModel findById(long id);
+}
