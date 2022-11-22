9e0c3e3e15688f34c857afc5961eb89fef550750
Damir Krancpiler <d.krancpiler@reply.de>
2022-11-22 12:09:56 +0100

    fixed get racing teams

--- src/main/java/com/comsysto/training9/racecardemo/repositories/DummyRacingTeamDataService.java
+++ src/main/java/com/comsysto/training9/racecardemo/repositories/DummyRacingTeamDataService.java
@@ -3,22 +3,25 @@ package com.comsysto.training9.racecardemo.repositories;
 import com.comsysto.training9.racecardemo.controllers.model.RacingTeamModel;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.comsysto.training9.racecardemo.services.RacingTeamDataService;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class RacingTeamRepository {
+@Component
+public class DummyRacingTeamDataService implements RacingTeamDataService {
     private List<RacingTeamModel> racingTeamModelList = new ArrayList<>();
-
+    @Override
     public RacingTeamModel save(RacingTeamModel racingTeamModel){
         racingTeamModelList.add(racingTeamModel);
         return racingTeamModel;
     }
-
+    @Override
     public List<RacingTeamModel> findAll() {
 
         return racingTeamModelList;
     }
-
+    @Override
     public RacingTeamModel findById(long id) {
         for(RacingTeamModel racingTeamRepository: racingTeamModelList){
             if(racingTeamRepository.getId().equals(id)){
--- src/main/java/com/comsysto/training9/racecardemo/services/JpaRacingTeamDataService.java
+++ src/main/java/com/comsysto/training9/racecardemo/services/JpaRacingTeamDataService.java
@@ -3,6 +3,8 @@ package com.comsysto.training9.racecardemo.services;
 import com.comsysto.training9.racecardemo.controllers.model.RacingTeamModel;
 import com.comsysto.training9.racecardemo.repositories.RacingTeamRealRepository;
 import java.util.List;
+
+import com.comsysto.training9.racecardemo.repositories.entity.RacingTeamEntity;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,8 +20,7 @@ public class JpaRacingTeamDataService implements RacingTeamDataService{
 
   @Override
   public RacingTeamModel save(RacingTeamModel racingTeamModel) {
-
-    return null;
+    return toRacingTeamModel(racingTeamRealRepository.save(toRacingTeamEntity(racingTeamModel)));
   }
 
   @Override
@@ -30,5 +31,16 @@ public class JpaRacingTeamDataService implements RacingTeamDataService{
   @Override
   public RacingTeamModel findById(long id) {
     return null;
+  }
+
+  private RacingTeamEntity toRacingTeamEntity(RacingTeamModel racingTeamModel) {
+    var entity = new RacingTeamEntity();
+    entity.setId(racingTeamModel.getId());
+    entity.setName(racingTeamModel.getName());
+    return entity;
+  }
+
+  private static RacingTeamModel toRacingTeamModel(RacingTeamEntity racingTeamEntity) {
+    return new RacingTeamModel(racingTeamEntity.getId(), racingTeamEntity.getName());
   }
 }
