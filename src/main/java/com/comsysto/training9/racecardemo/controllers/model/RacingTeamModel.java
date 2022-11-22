0be52c61b05bed9c38b931356ac1ea6360ec320d
Vigerl, Michael (StMELF-Extern Fa. ComsystoReply) <Michael.Vigerl@stmelf.bayern.de>
2022-11-22 15:49:07 +0100

    fix tests

--- src/main/java/com/comsysto/training9/racecardemo/controllers/model/RacingTeamModel.java
+++ src/main/java/com/comsysto/training9/racecardemo/controllers/model/RacingTeamModel.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import lombok.Value;
 
 @Value
-
 public class RacingTeamModel {
 
     Long id;
--- src/test/java/com/comsysto/training9/racecardemo/services/RacingTeamDataServiceTest.java
+++ src/test/java/com/comsysto/training9/racecardemo/services/RacingTeamDataServiceTest.java
@@ -6,6 +6,7 @@ import com.comsysto.training9.racecardemo.controllers.model.RacingTeamModel;
 import com.comsysto.training9.racecardemo.repositories.RacingTeamRealRepository;
 import com.comsysto.training9.racecardemo.repositories.entity.RacingTeamEntity;
 import org.aspectj.lang.annotation.Before;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -13,28 +14,35 @@ import org.mockito.Mockito;
 
 class RacingTeamDataServiceTest {
 
-  private JpaRacingTeamDataService jpaRacingTeamDataService;
+    private JpaRacingTeamDataService jpaRacingTeamDataService;
 
-  private RacingTeamRealRepository racingTeamRealRepository;
+    private RacingTeamRealRepository racingTeamRealRepository;
+    private final RacingTeamModel racingTeamModel = new RacingTeamModel(9999L, "name");
 
-  @BeforeEach
-  void initialize() {
-    racingTeamRealRepository = Mockito.mock(RacingTeamRealRepository.class);
-    jpaRacingTeamDataService = new JpaRacingTeamDataService(racingTeamRealRepository);
-  }
+    @BeforeEach
+    void initialize() {
+        racingTeamRealRepository = Mockito.mock(RacingTeamRealRepository.class);
+        jpaRacingTeamDataService = new JpaRacingTeamDataService(racingTeamRealRepository);
+    }
 
 
-  @Test
-  void testSave(){
-    RacingTeamModel racingTeamModel = new RacingTeamModel(9999L,"name");
-    Mockito.when(racingTeamRealRepository.save(toRacingTeamEntity(racingTeamModel))).thenReturn(toRacingTeamEntity(racingTeamModel));
-    jpaRacingTeamDataService.save(racingTeamModel);
+    @Test
+    void testSave() {
+        //given
+        Mockito.when(racingTeamRealRepository.save(toRacingTeamEntity(racingTeamModel)))
+                .thenReturn(toRacingTeamEntity(racingTeamModel));
 
-  }
-  private RacingTeamEntity toRacingTeamEntity(RacingTeamModel racingTeamModel) {
-    var entity = new RacingTeamEntity();
-    entity.setId(racingTeamModel.getId());
-    entity.setName(racingTeamModel.getName());
-    return entity;
-  }
+        //when
+        RacingTeamModel res = jpaRacingTeamDataService.save(racingTeamModel);
+
+        //then
+        Assertions.assertThat(res).isEqualTo(racingTeamModel);
+    }
+
+    private RacingTeamEntity toRacingTeamEntity(RacingTeamModel racingTeamModel) {
+        var entity = new RacingTeamEntity();
+        entity.setId(racingTeamModel.getId());
+        entity.setName(racingTeamModel.getName());
+        return entity;
+    }
 }
